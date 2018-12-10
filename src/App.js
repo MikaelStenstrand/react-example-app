770c115053aada3fa0063d516d1d62970c5f8160
Mikael Stenstrand <mste@mickee.local>
2018-12-10 16:07:12 +0200

    changed app.js with master changes

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <p>THIS HAS BEEN CHANGED IN MASTER</p>
           <a
             className="App-link"
             href="https://reactjs.org"
